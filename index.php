425aa63a2a5bbf9e7c8456b393ce91cb504e9c0d
Mateus Henrique <mateusteccruz@gmail.com>
2023-03-15 23:53:18 -0300

    Primeira versão

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+                    <p>
+                    <label>Número 1:</label>
+                    <input type="number" name="numero1">
+                    </p>
+                    <p>
+                    <label>Número 2:</label>
+                    <input type="number" name="numero2">
+                    </p>
+                    <p>
+                    <label>Operador:</label>
+                    <select name="operador">
+                    <option value="+"> + </option>
+                    <option value="-"> - </option>
+                    <option value="*"> * </option>
+                    <option value="/"> / </option>
+                    </select>
+                    </p>
+                    <button>Enviar</button>
+                    </form>
+                    <p>
+                    <a href="listar.php">Listar dados</a>
+                    </p>
+</body>
+</html>
+      
+                
